b5c2218caa814b5262d28136b111f9c862ce185a
くぁーりぃ <kaito6343@gmail.com>
2022-04-27 12:34:40 +0900

    basic setup

---
+++ src/main/java/gtexpert/client/ClientProxy.java
@@ -0,0 +1,20 @@
+package gtexpert.client;
+
+import gtexpert.common.CommonProxy;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod.EventBusSubscriber(Side.CLIENT)
+public class ClientProxy extends CommonProxy {
+    @Override
+    public void preInit(FMLPreInitializationEvent e) {
+        super.preInit(e);
+    }
+
+    @SubscribeEvent
+    public static void registerModels(ModelRegistryEvent event) {
+    }
+}
