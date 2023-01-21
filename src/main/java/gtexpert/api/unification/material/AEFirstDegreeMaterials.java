df072b6ae1bb4028c9bf39d4623adbfa4651031c
tier940 <tsubasa.ito@relightings.com>
2023-01-21 11:13:48 +0900

    [fix] AEFirstDegreeMaterials.java

--- src/main/java/gtexpert/api/unification/material/AEFirstDegreeMaterials.java
+++ src/main/java/gtexpert/api/unification/material/AEFirstDegreeMaterials.java
@@ -20,20 +20,18 @@ public class AEFirstDegreeMaterials {
     public static void init() {
         // Fluix
         FLUIX = new Material.Builder(25101, "fluix")
-                .dust().fluid()
-                .fluidTemp(1200)
-                .color(0x846994)
-                .iconSet(MaterialIconSet.QUARTZ)
+                .dust()
+                .fluid().fluidTemp(1200)
+                .color(0x846994).iconSet(MaterialIconSet.QUARTZ)
                 .flags(GENERATE_PLATE, GENERATE_LENS, DISABLE_DECOMPOSITION)
                 .components(Silicon, 2, Oxygen, 4, Redstone, 1)
                 .build();
 
         // Fluix Alloy
         FLUIX_ALLOY = new Material.Builder(25102, "fluix_alloy")
-                .dust().ingot().fluid()
-                .fluidTemp(1200)
-                .color(0x4A3954)
-                .iconSet(MaterialIconSet.SHINY)
+                .dust().ingot()
+                .fluid().fluidTemp(1200)
+                .color(0x4A3954).iconSet(MaterialIconSet.SHINY)
                 .flags(DISABLE_DECOMPOSITION)
                 .blastTemp(5400, GasTier.HIGHER, 1920, 1072)
                 .components(FLUIX, 1, Steel, 1)
@@ -41,10 +39,9 @@ public class AEFirstDegreeMaterials {
 
         // Charged Certus Quartz
         CHARGED_CERTUS_QUARTZ = new Material.Builder(25103, "charged_certus_quartz")
-                .dust().fluid()
-                .fluidTemp(1200)
-                .color(0xCFDAFF)
-                .iconSet(MaterialIconSet.QUARTZ)
+                .dust()
+                .fluid().fluidTemp(1200)
+                .color(0xCFDAFF).iconSet(MaterialIconSet.QUARTZ)
                 .flags(GENERATE_PLATE, GENERATE_LENS, DISABLE_DECOMPOSITION)
                 .components(Silicon, 1, Oxygen, 2)
                 .build();
