a120a01c071e73567ddc035e1b0a5d697b90bd22
tier940 <tsubasa.ito@relightings.com>
2023-04-08 12:26:42 +0900

    update gtceu to 2.6.0

--- src/main/java/gtexpert/api/recipes/GTERecipeMaps.java
+++ src/main/java/gtexpert/api/recipes/GTERecipeMaps.java
@@ -14,7 +14,6 @@ import stanhebben.zenscript.annotations.ZenProperty;
 @ZenExpansion("mods.gregtech.recipe.RecipeMaps")
 @ZenRegister
 public class GTERecipeMaps {
-
     @ZenProperty
     public static final RecipeMap<SimpleRecipeBuilder> EXTREME_MIXER_RECIPES = new RecipeMap<>("extreme_mixer", 9, 1, 0, 1, new SimpleRecipeBuilder(), false)
             .setSlotOverlay(false, false, GuiTextures.DUST_OVERLAY)
@@ -23,18 +22,18 @@ public class GTERecipeMaps {
             .setSound(GTSoundEvents.MIXER);
 
     @ZenProperty
+    public static final RecipeMap<SimpleRecipeBuilder> VIAL_EXTRACTOR_RECIPES = new RecipeMap<>("vial_extractor", 1, 4, 0, 1, new SimpleRecipeBuilder(), false)
+            .setSlotOverlay(false, false, GuiTextures.EXTRACTOR_OVERLAY)
+            .setProgressBar(GuiTextures.PROGRESS_BAR_EXTRACT, ProgressWidget.MoveType.HORIZONTAL)
+            .setSound(GTSoundEvents.COMPRESSOR);
+
+    @ZenProperty
     public static final RecipeMap<SimpleRecipeBuilder> SAWMill_RECIPES = new RecipeMap<>("sawmill", 2, 2, 1, 0, new SimpleRecipeBuilder(), false)
             .setSlotOverlay(false, false, GuiTextures.SAWBLADE_OVERLAY)
             .setSlotOverlay(true, false, false, GuiTextures.CUTTER_OVERLAY)
             .setSlotOverlay(true, false, true, GuiTextures.DUST_OVERLAY)
             .setProgressBar(GuiTextures.PROGRESS_BAR_SLICE, ProgressWidget.MoveType.HORIZONTAL)
             .setSound(GTSoundEvents.CHAINSAW_TOOL);
-
-    @ZenProperty
-    public static final RecipeMap<SimpleRecipeBuilder> VIAL_EXTRACTOR_RECIPES = new RecipeMap<>("vial_extractor", 1, 4, 0, 1, new SimpleRecipeBuilder(), false)
-            .setSlotOverlay(false, false, GuiTextures.EXTRACTOR_OVERLAY)
-            .setProgressBar(GuiTextures.PROGRESS_BAR_EXTRACT, ProgressWidget.MoveType.HORIZONTAL)
-            .setSound(GTSoundEvents.COMPRESSOR);
 
     @ZenProperty
     public static final RecipeMap<SimpleRecipeBuilder> VOID_ORE_MINER_RECIPES = new RecipeMap<>("void_ore_miner", 1, 1, 2, 0, new SimpleRecipeBuilder(), false)
--- src/main/java/gtexpert/client/GTETextures.java
+++ src/main/java/gtexpert/client/GTETextures.java
@@ -1,27 +1,21 @@
 package gtexpert.client;
 
-import gregtech.client.renderer.texture.cube.OrientedOverlayRenderer;
-import gregtech.client.renderer.texture.cube.SimpleOverlayRenderer;
-import gregtech.client.renderer.texture.cube.SimpleSidedCubeRenderer;
+import gregtech.client.renderer.texture.cube.*;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.relauncher.Side;
-import static gregtech.client.renderer.texture.cube.OrientedOverlayRenderer.OverlayFace.*;
-
-import org.jetbrains.annotations.NotNull;
 
 @Mod.EventBusSubscriber(modid = "gtexpert", value = Side.CLIENT)
 public class GTETextures {
     public static SimpleOverlayRenderer SAWMILL_CASING;
     public static OrientedOverlayRenderer SAWMILL_OVERLAY;
-    public static @NotNull OrientedOverlayRenderer VIAL_EXTRACTOR_OVERLAY = new OrientedOverlayRenderer("vial_extractor", FRONT, SIDE, TOP);
-    public static @NotNull OrientedOverlayRenderer EXTREME_MIXER_OVERLAY = new OrientedOverlayRenderer("extreme_mixer", FRONT, SIDE, TOP);
+    public static OrientedOverlayRenderer EXTREME_MIXER_OVERLAY = new OrientedOverlayRenderer("extreme_mixer");
+    public static OrientedOverlayRenderer VIAL_EXTRACTOR_OVERLAY = new OrientedOverlayRenderer("vial_extractor");
     public static SimpleOverlayRenderer VOID_ORE_MINER_CASING;
     public static SimpleOverlayRenderer DRACONIUM_CASING;
     public static SimpleOverlayRenderer AWAKENED_DRACONIUM_CASING;
 
     public static void preInit() {
         SAWMILL_CASING = new SimpleOverlayRenderer("sawmill_casing");
-        SAWMILL_OVERLAY = new OrientedOverlayRenderer("sawmill", FRONT);
         VOID_ORE_MINER_CASING = new SimpleOverlayRenderer("void_ore_miner_casing");
         DRACONIUM_CASING = new SimpleOverlayRenderer("draconium_casing");
         AWAKENED_DRACONIUM_CASING = new SimpleOverlayRenderer("awakened_draconium_casing");
