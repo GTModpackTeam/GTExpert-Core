68431b0a8e0bb5f1ef95ece4ad2725a43b8955fe
tier940 <tsubasa.ito@relightings.com>
2023-04-15 21:33:12 +0900

    add GTFORecipeLoader

--- src/main/java/gtexpert/loaders/recipe/GTFORecipeLoader.java
+++ src/main/java/gtexpert/loaders/recipe/GTFORecipeLoader.java
@@ -1,35 +1,20 @@
 package gtexpert.loaders.recipe;
 
-import crazypants.enderio.base.fluid.Fluids;
-import gregtech.api.items.OreDictNames;
 import gregtech.api.recipes.GTRecipeHandler;
-import gregtech.api.recipes.ModHandler;
 import gregtech.api.recipes.RecipeMaps;
-import gregtech.api.unification.OreDictUnifier;
-import gregtech.api.unification.stack.UnificationEntry;
-import gregtech.api.util.GTUtility;
-import gregtech.common.metatileentities.MetaTileEntities;
 import gregtechfoodoption.GTFOMaterialHandler;
-import gtexpert.api.recipes.GTERecipeMaps;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.oredict.OreDictionary;
-import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static gregtech.api.GTValues.*;
 import static gregtech.api.unification.material.Materials.*;
-import static gregtech.api.unification.ore.OrePrefix.*;
-import static gregtech.common.items.MetaItems.*;
 
-public class GTFOOverrideRecipeLoader {
+public class GTFORecipeLoader {
     public static void init() {
         // craftSludge
         OreDictionary.registerOre("craftSludge", new ItemStack(Items.PORKCHOP));
