6961fa8568189731af5fce4307cd5a9c92c29abd
tier940 <tsubasa.ito@relightings.com>
2023-04-28 20:49:43 +0900

    GTFOの木材加工をGT基準に修正

--- src/main/java/gtexpert/common/GTEConfigHolder.java
+++ src/main/java/gtexpert/common/GTEConfigHolder.java
@@ -2,7 +2,14 @@ package gtexpert.common;
 
 import gtexpert.api.GTEValues;
 import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.Config.*;
 
 @Config(modid = GTEValues.MODID)
-public class GTEConfigHolder {}
+public class GTEConfigHolder {
+    @Config.Comment({"Making Planks and Sticks even more difficult.",
+            "CEu's nerfWoodCrafting to true to reflect.", "Default: false"})
+    public static boolean moreNerfWoodCrafting = false;
+
+    @Config.Comment({"Change to a recipe using Assembly Line.",
+            "CEu's enableHighTierSolars to true to reflect.", "Default: false"})
+    public static boolean hardSolarPanel = false;
+}
