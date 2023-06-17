d842e1303eed04b09e2fee51f78a6fa4cf487635
tier940 <tsubasa.ito@relightings.com>
2023-06-17 14:46:39 +0900

    GTEUtils#getModFluidを追加してGTFOの依存を除去 (#92)

--- src/main/java/gtexpert/api/util/GTEUtils.java
+++ src/main/java/gtexpert/api/util/GTEUtils.java
@@ -2,6 +2,8 @@ package gtexpert.api.util;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,5 +23,13 @@ public class GTEUtils {
 
     public static @NotNull ItemStack getModItem(String modID, String itemName, int amount, int meta, NBTTagCompound nbt) {
         return GameRegistry.makeItemStack(String.format("%s:%s", modID, itemName), meta, amount, nbt != null ? nbt.toString() : null);
+    }
+
+    public static @NotNull FluidStack getModFluid(String fluidName) {
+        return FluidRegistry.getFluidStack(fluidName, 1000);
+    }
+
+    public static @NotNull FluidStack getModFluid(String fluidName, int amount) {
+        return FluidRegistry.getFluidStack(fluidName, amount);
     }
 }
