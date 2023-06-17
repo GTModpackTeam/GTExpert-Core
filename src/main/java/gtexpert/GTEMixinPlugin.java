45c96ea8a3420f0cd9d6d903bc06be12d6de8825
tier940 <tsubasa.ito@relightings.com>
2023-06-17 14:45:23 +0900

    getModFluidを追加してGTFOの依存を除去

--- src/main/java/gtexpert/GTEMixinPlugin.java
+++ src/main/java/gtexpert/GTEMixinPlugin.java
@@ -1,8 +1,8 @@
 package gtexpert;
 
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.Map;
 
 @IFMLLoadingPlugin.MCVersion("1.12.2")
