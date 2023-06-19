b217c38365c5fbcb9a182c59210e649629b9907f
tier940 <tsubasa.ito@relightings.com>
2023-06-20 01:14:55 +0900

    frametimeを1から3に変更とwireとpipeのテスト

--- src/main/java/gtexpert/api/unification/material/GTEFirstDegreeMaterials.java
+++ src/main/java/gtexpert/api/unification/material/GTEFirstDegreeMaterials.java
@@ -4,9 +4,8 @@ import gregtech.api.fluids.fluidType.FluidTypes;
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.properties.BlastProperty.GasTier;
 
-import gregicality.multiblocks.api.fluids.fluidType.GCYMFluidTypes;
-
 import gtexpert.api.unification.material.info.GTEMaterialIconSet;
+import net.minecraftforge.fml.common.Loader;
 
 import static gregtech.api.GTValues.*;
 import static gregtech.api.unification.material.Materials.*;
@@ -50,16 +49,24 @@ public class GTEFirstDegreeMaterials {
                 .flags(NO_WORKING)
                 .build();
 
+        // Neutronium
+//        if (Loader.isModLoaded("avaritia")) {
+//            Neutronium.setMaterialRGB(0x000000);
+//            Neutronium.setMaterialIconSet(GTEMaterialIconSet.NEUTRONIUM);
+//            Neutronium.addFlags(GENERATE_SMALL_GEAR, GENERATE_FOIL, GENERATE_RING, GENERATE_ROTOR);
+//        }
+
         // Infinity
-        Infinity = new Material.Builder(24004, "infinity")
+        Infinity = new Material.Builder(24005, "infinity")
                 .dust(7).ingot(7)
-                .fluid(GCYMFluidTypes.MOLTEN, false).fluidTemp(10800)
+                .fluid(FluidTypes.LIQUID, false).fluidTemp(10800)
                 .iconSet(GTEMaterialIconSet.INFINITY)
                 .flags(EXT2_METAL, GENERATE_DENSE, GENERATE_GEAR, GENERATE_SMALL_GEAR, GENERATE_FRAME,
-                        GENERATE_FINE_WIRE, GENERATE_RING, GENERATE_ROTOR, GENERATE_ROUND,
-                        GENERATE_SPRING, GENERATE_SPRING_SMALL, EXCLUDE_BLOCK_CRAFTING_BY_HAND_RECIPES,
-                        EXCLUDE_BLOCK_CRAFTING_RECIPES, EXCLUDE_PLATE_COMPRESSOR_RECIPE, DISABLE_DECOMPOSITION,
-                        NO_SMASHING, NO_SMELTING)
+                        GENERATE_FOIL, GENERATE_FINE_WIRE, GENERATE_RING, GENERATE_ROTOR, // GENERATE_ROUND,
+                        GENERATE_BOLT_SCREW, GENERATE_SPRING, EXCLUDE_BLOCK_CRAFTING_BY_HAND_RECIPES,
+                        EXCLUDE_BLOCK_CRAFTING_RECIPES, EXCLUDE_PLATE_COMPRESSOR_RECIPE, NO_SMASHING, NO_SMELTING)
+                .fluidPipeProperties(150_000, 7500, true, true, true, true)
+                .cableProperties(V[UEV], 32, 0, true, 1) //TODO: Texture is not applied[gregtech.client.renderer.pipe]
                 .element(If)
                 .build();
     }
