d49822e3c0984a86948779c4232bb71848957807
sjcl <bebe@sjcl.me>
2023-06-25 23:21:37 +0900

    Mixinのデバッグ用フラグを追加

--- src/main/java/gtexpert/GTEMixinLoader.java
+++ src/main/java/gtexpert/GTEMixinLoader.java
@@ -1,14 +1,18 @@
 package gtexpert;
 
 import com.google.common.collect.Lists;
+import net.minecraftforge.fml.common.Loader;
 import zone.rong.mixinbooter.ILateMixinLoader;
 
+import java.util.Collections;
 import java.util.List;
 
 public class GTEMixinLoader implements ILateMixinLoader {
 
     @Override
     public List<String> getMixinConfigs() {
-        return Lists.newArrayList("mixins.gtexpert.json");
+        if (Loader.isModLoaded("ezstorage"))
+            return Lists.newArrayList("mixins.gtexpert.json");
+        return Collections.emptyList();
     }
 }
