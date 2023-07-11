479e6436782d19cccf25b6373f8a85f59acf733f
sjcl <bebe@sjcl.me>
2023-07-11 14:20:22 +0900

    Refactor MixinRecipeMaps

--- src/main/java/gtexpert/mixin/MixinRecipeMaps.java
+++ src/main/java/gtexpert/mixin/MixinRecipeMaps.java
@@ -1,27 +1,23 @@
 package gtexpert.mixin;
 
-import gregtech.api.recipes.RecipeBuilder;
-import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.RecipeMaps;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(RecipeMaps.class)
 public abstract class MixinRecipeMaps {
 
-    @Redirect(method = "<clinit>", at = @At(value = "NEW", target = "gregtech/api/recipes/RecipeMap", remap = false))
-    private static <R extends RecipeBuilder<R>> RecipeMap<?> redirectStaticBlock(String unlocalizedName, int maxInputs,
-                                                                                 int maxOutputs, int maxFluidInputs,
-                                                                                 int maxFluidOutputs,
-                                                                                 R defaultRecipeBuilder,
-                                                                                 boolean isHidden) {
+    @ModifyArgs(method = "<clinit>",
+                at = @At(value = "INVOKE",
+                         target = "Lgregtech/api/recipes/RecipeMap;<init>(Ljava/lang/String;IIIILgregtech/api/recipes/RecipeBuilder;Z)V",
+                         remap = false))
+    private static void modifyArgsStaticBlockRecipeMap(Args args) {
+        String unlocalizedName = args.get(0);
         if (unlocalizedName.equals("vacuum_freezer")) {
-            return new RecipeMap<>(unlocalizedName, maxInputs, maxOutputs, maxFluidInputs, 2, defaultRecipeBuilder,
-                    isHidden);
+            args.set(4, 2);
         }
-        return new RecipeMap<>(unlocalizedName, maxInputs, maxOutputs, maxFluidInputs, maxFluidOutputs,
-                defaultRecipeBuilder, isHidden);
     }
 }
