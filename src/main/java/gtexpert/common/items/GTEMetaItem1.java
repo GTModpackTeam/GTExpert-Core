5a17a4fe63cc4d1998e152e745dde81dabd99474
tier940 <tsubasa.ito@relightings.com>
2023-10-22 00:35:42 +0900

    初歩的なモーターなどを追加

--- src/main/java/gtexpert/common/items/GTEMetaItem1.java
+++ src/main/java/gtexpert/common/items/GTEMetaItem1.java
@@ -1,6 +1,9 @@
 package gtexpert.common.items;
 
 import gregtech.api.items.metaitem.StandardMetaItem;
+import gregtech.common.items.behaviors.TooltipBehavior;
+import gtexpert.common.GTEConfigHolder;
+import net.minecraft.client.resources.I18n;
 
 import static gtexpert.common.items.GTEMetaItems.*;
 
@@ -14,5 +17,23 @@ public class GTEMetaItem1 extends StandardMetaItem {
     public void registerSubItems() {
         GTE_ME_FAKE_COMPONENT = addItem(0, "gte_me_fake_component");
         MATRIX_CORE = addItem(1, "matrix_core");
+
+        // Primitive modules: ID 2-6
+        if (GTEConfigHolder.ceuOverride.enablePrimitiveCovers) {
+            PRIMITIVE_MOTOR = addItem(2, "primitive.motor");
+            PRIMITIVE_PUMP = addItem(3, "primitive.pump").addComponents(new TooltipBehavior(lines -> {
+                lines.add(I18n.format("metaitem.electric.pump.tooltip"));
+                lines.add(I18n.format("gregtech.universal.tooltip.fluid_transfer_rate", 1280 / 4));
+            }));
+            PRIMITIVE_CONVEYOR = addItem(4, "primitive.conveyor").addComponents(new TooltipBehavior(lines -> {
+                lines.add(I18n.format("metaitem.conveyor.module.tooltip"));
+                lines.add(I18n.format("gregtech.universal.tooltip.item_transfer_rate", 2));
+            }));
+            PRIMITIVE_PISTON = addItem(5, "primitive.piston");
+            PRIMITIVE_ROBOT_ARM = addItem(6, "primitive.robot.arm").addComponents(new TooltipBehavior(lines -> {
+                lines.add(I18n.format("metaitem.robot.arm.tooltip"));
+                lines.add(I18n.format("gregtech.universal.tooltip.item_transfer_rate", 2));
+            }));
+        }
     }
 }
--- src/main/java/gtexpert/common/items/GTEMetaItems.java
+++ src/main/java/gtexpert/common/items/GTEMetaItems.java
@@ -1,7 +1,14 @@
 package gtexpert.common.items;
 
+import gregtech.api.GTValues;
 import gregtech.api.items.armor.ArmorMetaItem;
 import gregtech.api.items.metaitem.MetaItem;
+import gregtech.common.covers.CoverConveyor;
+import gregtech.common.covers.CoverPump;
+import gregtech.common.covers.CoverRoboticArm;
+
+import static gregtech.common.covers.CoverBehaviors.registerBehavior;
+import static gtexpert.api.util.GTEUtility.gteId;
 
 public final class GTEMetaItems {
 
@@ -9,6 +16,11 @@ public final class GTEMetaItems {
 
     public static MetaItem<?>.MetaValueItem GTE_ME_FAKE_COMPONENT;
     public static MetaItem<?>.MetaValueItem MATRIX_CORE;
+    public static MetaItem<?>.MetaValueItem PRIMITIVE_MOTOR;
+    public static MetaItem<?>.MetaValueItem PRIMITIVE_PUMP;
+    public static MetaItem<?>.MetaValueItem PRIMITIVE_CONVEYOR;
+    public static MetaItem<?>.MetaValueItem PRIMITIVE_PISTON;
+    public static MetaItem<?>.MetaValueItem PRIMITIVE_ROBOT_ARM;
 
     public static ArmorMetaItem<?>.ArmorMetaValueItem PISTON_BOOTS;
 
