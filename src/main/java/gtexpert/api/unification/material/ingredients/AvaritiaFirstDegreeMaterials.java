43ffd3220d81b453438804cc7da7b10fbab1d630
tier940 <tsubasa.ito@relightings.com>
2023-12-01 23:38:15 +0900

    Infinityツールを追加

--- src/main/java/gtexpert/api/unification/material/ingredients/AvaritiaFirstDegreeMaterials.java
+++ src/main/java/gtexpert/api/unification/material/ingredients/AvaritiaFirstDegreeMaterials.java
@@ -7,8 +7,8 @@ import static gregtech.api.util.GTUtility.gregtechId;
 import static gtexpert.api.unification.GTEElements.*;
 import static gtexpert.api.unification.material.GTEMaterials.*;
 
-import gregtech.api.fluids.FluidBuilder;
 import gregtech.api.unification.material.Material;
+import gregtech.api.unification.material.properties.ToolProperty;
 
 import gtexpert.api.unification.material.info.GTEMaterialIconSet;
 
@@ -23,12 +23,13 @@ public class AvaritiaFirstDegreeMaterials {
         // Infinity
         Infinity = new Material.Builder(24186, gregtechId("infinity"))
                 .dust(7).ingot(7)
-                .liquid(new FluidBuilder().temperature(10800))
                 .iconSet(GTEMaterialIconSet.INFINITY)
                 .flags(EXT2_METAL, GENERATE_DENSE, GENERATE_GEAR, GENERATE_SMALL_GEAR, GENERATE_FRAME,
-                        GENERATE_FOIL, GENERATE_FINE_WIRE, GENERATE_RING, GENERATE_ROTOR, // GENERATE_ROUND,
+                        GENERATE_FOIL, GENERATE_FINE_WIRE, GENERATE_RING, GENERATE_ROTOR, GENERATE_ROUND,
                         GENERATE_BOLT_SCREW, GENERATE_SPRING, EXCLUDE_BLOCK_CRAFTING_BY_HAND_RECIPES,
                         EXCLUDE_BLOCK_CRAFTING_RECIPES, EXCLUDE_PLATE_COMPRESSOR_RECIPE, NO_SMASHING, NO_SMELTING)
+                .toolStats(ToolProperty.Builder.of(200.0F, 140.0F, 65535, 6).attackSpeed(0.5F).enchantability(38)
+                        .magnetic().unbreakable().build())
                 .fluidPipeProperties(10_000_000, 200_000, true, true, true, true)
                 .cableProperties(V[MAX], 8192, 0, true)
                 .element(If)
