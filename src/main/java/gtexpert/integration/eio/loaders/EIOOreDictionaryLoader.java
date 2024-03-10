d5fe6d9c48d788067fe509afeceec9e56f1c5c15
sysnote8main <sysnote8main@gmail.com>
2024-03-10 12:14:19 +0900

    refactor: register oreDicts

--- src/main/java/gtexpert/integration/eio/loaders/EIOOreDictionaryLoader.java
+++ src/main/java/gtexpert/integration/eio/loaders/EIOOreDictionaryLoader.java
@@ -4,6 +4,8 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
+import gtexpert.api.util.GTEUtility;
+
 import crazypants.enderio.base.init.ModObject;
 import crazypants.enderio.base.material.material.Material;
 import crazypants.enderio.endergy.init.EndergyObject;
@@ -12,29 +14,35 @@ public class EIOOreDictionaryLoader {
 
     public static void init() {
         // Gears
-        OreDictionary.registerOre("gearEnergeticAlloy", Material.GEAR_ENERGIZED.getStack());
+        GTEUtility.registerOre("gearEnergeticAlloy", Material.GEAR_ENERGIZED.getStack());
         OreDictionary.registerOre("gearVibrantAlloy", Material.GEAR_VIBRANT.getStack());
 
         // craftNutrientDistillation
-        OreDictionary.registerOre("craftNutrientDistillation", new ItemStack(Items.PORKCHOP));
-        OreDictionary.registerOre("craftNutrientDistillation", new ItemStack(Items.BEEF));
-        OreDictionary.registerOre("craftNutrientDistillation", new ItemStack(Items.CHICKEN));
-        OreDictionary.registerOre("craftNutrientDistillation", new ItemStack(Items.RABBIT));
-        OreDictionary.registerOre("craftNutrientDistillation", new ItemStack(Items.MUTTON));
-        OreDictionary.registerOre("craftNutrientDistillation", new ItemStack(Items.FISH));
-        OreDictionary.registerOre("craftNutrientDistillation", new ItemStack(Items.FISH, 1, 1));
-        OreDictionary.registerOre("craftNutrientDistillation", new ItemStack(Items.FISH, 1, 2));
+        GTEUtility.registerOre(
+                "craftNutrientDistillation",
+                new ItemStack(Items.PORKCHOP),
+                new ItemStack(Items.BEEF),
+                new ItemStack(Items.CHICKEN),
+                new ItemStack(Items.RABBIT),
+                new ItemStack(Items.MUTTON),
+                new ItemStack(Items.FISH),
+                new ItemStack(Items.FISH, 1, 1),
+                new ItemStack(Items.FISH, 1, 2));
 
         // craftHootch
-        OreDictionary.registerOre("craftHootch", new ItemStack(Items.WHEAT_SEEDS));
-        OreDictionary.registerOre("craftHootch", new ItemStack(Items.DYE, 1, 3));
-        OreDictionary.registerOre("craftHootch", new ItemStack(Items.BEETROOT_SEEDS));
-        OreDictionary.registerOre("craftHootch", new ItemStack(Items.PUMPKIN_SEEDS));
-        OreDictionary.registerOre("craftHootch", new ItemStack(Items.MELON_SEEDS));
-        OreDictionary.registerOre("craftHootch", new ItemStack(Items.POISONOUS_POTATO));
+        GTEUtility.registerOre(
+                "craftHootch",
+                new ItemStack(Items.WHEAT_SEEDS),
+                new ItemStack(Items.DYE, 1, 3),
+                new ItemStack(Items.BEETROOT_SEEDS),
+                new ItemStack(Items.PUMPKIN_SEEDS),
+                new ItemStack(Items.MELON_SEEDS),
+                new ItemStack(Items.POISONOUS_POTATO));
 
         // eio.capacitor
-        OreDictionary.registerOre("craftCapacitorEIO", new ItemStack(ModObject.itemBasicCapacitor.getItemNN()));
-        OreDictionary.registerOre("craftCapacitorEIO", new ItemStack(EndergyObject.itemCapacitorSilver.getItemNN(), 1));
+        GTEUtility.registerOre(
+                "craftCapacitorEIO",
+                new ItemStack(ModObject.itemBasicCapacitor.getItemNN()),
+                new ItemStack(EndergyObject.itemCapacitorSilver.getItemNN(), 1));
     }
 }
