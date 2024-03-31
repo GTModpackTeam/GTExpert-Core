d603feedeaf809d56c4b45edf3dfbadcb06751e6
ko_no <90126004+MrKono@users.noreply.github.com>
2024-03-31 20:06:21 +0900

    cfg

--- src/main/java/gtexpert/integration/ffm/recipes/machines/FFMMachineRecipeManager.java
+++ src/main/java/gtexpert/integration/ffm/recipes/machines/FFMMachineRecipeManager.java
@@ -1,10 +1,15 @@
 package gtexpert.integration.ffm.recipes.machines;
 
+import gtexpert.integration.ffm.FFMConfigHolder;
+
 public class FFMMachineRecipeManager {
 
     public static void init() {
         CarpenterLoader.initBase();
+        if (FFMConfigHolder.hardForestryRecipe) {
+            CarpenterLoader.initHard();
+        } else {
         CarpenterLoader.initNormal();
-        CarpenterLoader.initHard();
+        }
     }
 }
