64e285c99e073efeee80d71a244032c791e771b0
tier940 <tier940@protonmail.com>
2025-01-19 21:39:37 +0900

    GTBeesMatrixに分離

--- src/main/java/com/github/gtexpert/core/integration/gendustry/recipes/GendustryBlocksRecipe.java
+++
@@ -1,6 +0,0 @@
-package com.github.gtexpert.core.integration.gendustry.recipes;
-
-public class GendustryBlocksRecipe {
-
-    public static void init() {}
-}
--- src/main/java/com/github/gtexpert/core/integration/gendustry/recipes/GendustryItemsRecipe.java
+++
@@ -1,6 +0,0 @@
-package com.github.gtexpert.core.integration.gendustry.recipes;
-
-public class GendustryItemsRecipe {
-
-    public static void init() {}
-}
