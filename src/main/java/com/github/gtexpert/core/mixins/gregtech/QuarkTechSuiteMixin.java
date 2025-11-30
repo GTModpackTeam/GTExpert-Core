895a1d942a44a5c484196b4b38e5e8981b2b8e9d
ko_no <90126004+MrKono@users.noreply.github.com>
2025-12-01 00:01:35 +0900

    Fix QuarkTechSuiteMixin: target

--- src/main/java/com/github/gtexpert/core/mixins/gregtech/QuarkTechSuiteMixin.java
+++ src/main/java/com/github/gtexpert/core/mixins/gregtech/QuarkTechSuiteMixin.java
@@ -13,7 +13,8 @@ import com.github.gtexpert.core.common.GTEConfigHolder;
 @Mixin(value = QuarkTechSuite.class, remap = false)
 public class QuarkTechSuiteMixin {
 
-    @Redirect(method = "onArmorTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/FoodStats;needFood()Z"))
+    @Redirect(method = "onArmorTick",
+              at = @At(value = "INVOKE", target = "Lnet/minecraft/util/FoodStats;func_75121_c()Z"))
     private boolean gteCore$onArmorTick(FoodStats foodStats) {
         if (GTEConfigHolder.ceuOverride.disableHelmetAutoEat) {
             return false;
